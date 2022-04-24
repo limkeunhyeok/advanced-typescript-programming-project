6079be3cd541eabcec104de59e1fa6463c89018a
limkeunhyeok <gorloom6425@naver.com>
2022-04-25 01:36:14 +0900

    타입스크립트 실전 프로젝트 개인 주소록 (1)

---
+++ personal-contacts-manager/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Container } from 'reactstrap';
+import PersonalDetails from './PersonalDetails';
+
+function App() {
+  return (
+    <Container>
+      <PersonalDetails></PersonalDetails>
+    </Container>
+  );
+}
+
+export default App;
---
+++ personal-contacts-manager/src/PersonalDetails.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Button, Col, Row } from 'reactstrap';
+
+const PersonalDetails: React.FunctionComponent = ({}) => {
+  return (
+    <Row>
+      <Col lg="8">{}</Col>
+    </Row>
+  );
+};
+
+export default PersonalDetails;
