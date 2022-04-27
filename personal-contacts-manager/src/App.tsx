9a66ffbeb3ed94a74fae57fbec3903f2220417e7
limkeunhyeok <gorloom6425@naver.com>
2022-04-27 19:45:23 +0900

    타입스크립트 실전 프로젝트 개인 주소록 (2)

--- personal-contacts-manager/src/App.tsx
+++ personal-contacts-manager/src/App.tsx
@@ -3,13 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import { Container } from 'reactstrap';
 import PersonalDetails from './PersonalDetails';
+import { IPersonState } from './State';
 
-function App() {
-  return (
-    <Container>
-      <PersonalDetails></PersonalDetails>
-    </Container>
-  );
+export default class App extends React.Component {
+  private defaultPerson: IPersonState = {
+    Address1: '',
+    Address2: null,
+    County: '',
+    DateOfBirth: new Date().toISOString().substring(0, 10),
+    FirstName: '',
+    LastName: '',
+    PersonId: '',
+    PhoneNumber: '',
+    Postcode: '',
+    Town: '',
+  };
+
+  public render() {
+    return (
+      <Container>
+        <PersonalDetails DefaultState={this.defaultPerson} />
+      </Container>
+    );
+  }
 }
-
-export default App;
