1cb43abb9d50a594d5c1e71659a2f9499d299171
limkeunhyeok <gorloom6425@naver.com>
2022-05-06 17:08:44 +0900

    타입스크립트 실전 프로젝트 socket을 이용한 채팅방

--- socket-io-chat/Server/Database/DataAccess/DataAccessBase.ts
+++ socket-io-chat/Server/Database/DataAccess/DataAccessBase.ts
@@ -7,5 +7,27 @@ export abstract class DataAccessBase<T extends mongoose.Document> {
     this.model = model;
   }
 
-  
+  Add(item: T): Promise<boolean> {
+    return new Promise<boolean>((callback, error) => {
+      this.model.create(item, (err: unknown, result: any) => {
+        if (err) {
+          error(err);
+        }
+        callback(!result);
+      })
+    })
+  }
+
+  GetAll(conditions: any, fields: any): Promise<unknown[]> {
+    return new Promise<T[]>((callback, error) => {
+      this.model.find(conditions, fields, (err: unknown, result: T[]) => {
+        if (err) {
+          error(err);
+        }
+        if (result) {
+          callback(result);
+        }
+      });
+    });
+  }
 }
