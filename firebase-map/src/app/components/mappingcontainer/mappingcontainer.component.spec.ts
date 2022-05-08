466c8fe017347e2d9536628ae492a2cb71b47810
limkeunhyeok <gorloom6425@naver.com>
2022-05-08 14:42:00 +0900

    타입스크립트 실전 프로젝트 파이어베이스를 사용한 클라우디 기반 앵귤러 지도

---
+++ firebase-map/src/app/components/mappingcontainer/mappingcontainer.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { MappingcontainerComponent } from "./mappingcontainer.component";
+
+describe("MappingcontainerComponent", () => {
+  let component: MappingcontainerComponent;
+  let fixture: ComponentFixture<MappingcontainerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [MappingcontainerComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MappingcontainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
